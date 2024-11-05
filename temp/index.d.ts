cb3d1582fe8cb82c982eed6f2950e0f2cff6ea19
richenlin <richenlin@gmail.com>
2024-11-05 13:48:32 +0800

    docs: api doc

---
+++ temp/index.d.ts
@@ -0,0 +1,3 @@
+export * from "./lib";
+export * as Helper from "./lib";
+//# sourceMappingURL=index.d.ts.map
---
+++ temp/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Helper = void 0;
+const tslib_1 = require("tslib");
+/*
+ * @Author: richen
+ * @Date: 2020-11-20 10:38:53
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-15 19:33:00
+ * @License: BSD (3-Clause)
+ * @Copyright (c) - <richenlin(at)gmail.com>
+ */
+tslib_1.__exportStar(require("./lib"), exports);
+exports.Helper = tslib_1.__importStar(require("./lib"));
+//# sourceMappingURL=index.js.map
